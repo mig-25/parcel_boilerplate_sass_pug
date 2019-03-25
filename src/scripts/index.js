d459d54d28ee41ef16e789d0986f0b6f073697e2
Sohail Hasware <kd3180@hotmail.com>
2019-03-25 14:48:51 +0100

    disable preittier for greet.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,5 @@
 import '../styles/main.scss'
+// eslint-disable-next-line prettier/prettier
 import {
   greet
 } from './greet'
@@ -6,4 +7,5 @@ import {
 // NOTE:: Can't name bundle from what I can see so attach to window. Not ideal but works 👍
 window.myPackage = {
   greet,
+  // eslint-disable-next-line prettier/prettier
 }
