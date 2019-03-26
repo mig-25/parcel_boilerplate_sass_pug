9c676093356a09b49fc4cd93b95369bb9b6e2c3d
Sohail Hasware <kd3180@hotmail.com>
2019-03-26 10:48:56 +0100

    add js lib folder and jarallax

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,9 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 import '../styles/main.scss'
+import {
+  jarallax
+} from './lib/jarallax'
 // eslint-disable-next-line prettier/prettier
 import {
   greet
---
+++ src/scripts/lib/jarallax.js
@@ -0,0 +1,11 @@
+// object-fit polyfill run
+objectFitImages();
+
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'), {
+noIos: false,
+});
+
+jarallax(document.querySelectorAll('.jarallax-keep-img'), {
+keepImg: true,
+});
